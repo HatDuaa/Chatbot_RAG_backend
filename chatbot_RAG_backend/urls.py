c18b36abd1141074edc2aafd4126673422ccc2d8
Hạt Bí <118889331+HatDuaa@users.noreply.github.com>
2024-11-23 22:14:20 +0700

    thay đổi đường dẫn

--- chatbot_RAG_backend/urls.py
+++ chatbot_RAG_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for chatbot_backend project.
+URL configuration for chatbot_RAG_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
